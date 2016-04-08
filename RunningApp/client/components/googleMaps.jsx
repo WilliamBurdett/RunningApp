6385263834cd4147321b0847b43ea2a766813a24
William Burdett <william.burdett1@gmail.com>
2016-04-08 07:41:41 -0700

    It's a map!

--- RunningApp/client/components/googleMaps.jsx
+++ RunningApp/client/components/googleMaps.jsx
@@ -26,34 +26,35 @@ export default class RunMap extends React.Component {
   }
 
   render () {
+    console.log("Render me!");
     return (
-    <section style={{height: "100%"}}>
-      <GoogleMapLoader
-        containerElement={
-          <div
-            {...this.props}
-            style={{
-              height: "100%",
-            }}
-          />
-        }
-        googleMapElement={
-          <GoogleMap
-            ref={(map) => console.log(map)}
-            defaultZoom={3}
-            defaultCenter={{lat: -25.363882, lng: 131.044922}}
-            onClick={this.handleMapClick}>
-            {this.state.markers.map((marker, index) => {
-              return (
-                <Marker
-                  {...marker}
-                  onRightclick={this.handleMarkerRightclick.bind(this, index)} />
-              );
-            })}
-          </GoogleMap>
-        }
-      />
-    </section>
+      <div className="flexContainerColumn flexFill ">
+      <section className="flexContainerColumn flexFill">
+        <GoogleMapLoader
+          containerElement={
+            <div
+              {...this.props}
+              className="flexContainerColumn flexFill "
+            />
+          }
+          googleMapElement={
+            <GoogleMap
+              ref={(map) => console.log(map)}
+              defaultZoom={3}
+              defaultCenter={{lat: -25.363882, lng: 131.044922}}
+              onClick={this.handleMapClick}>
+              {this.state.markers.map((marker, index) => {
+                return (
+                  <Marker
+                    {...marker}
+                    onRightclick={this.handleMarkerRightclick.bind(this, index)} />
+                );
+              })}
+            </GoogleMap>
+          }
+        />
+      </section>
+    </div>
   );
   }
 }
